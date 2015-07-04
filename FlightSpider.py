2543c53774caf6d22eeefb0cd7b566fb6a9c9795
Marsrocky <jianfei_mars@hotmail.com>
2015-07-05 00:46:55 +0800

    Add time information mining

--- FlightSpider.py
+++ FlightSpider.py
@@ -30,12 +30,28 @@ def searchInfo(searchUrl, num):
 	company = re.compile('<div class="flightAirline" title="(.*?)">').findall(html)
 	punctuality = re.compile('<div class="b_sumArea">(.*?)</div>').findall(html)
 	price = re.compile('class="price"><dfn>CNY</dfn><strong class="number">(.*?)</strong>').findall(html)
+	allTime = re.compile('<em class="number">(.*?)</em>').findall(html)
+
+
+	depTime = []; fliTime = []; lanTime = []
+	#Classify the time
+	timeFlag = 0
+	for item in allTime:
+		if 'h' in item:
+			fliTime.append(item)
+		elif ':' in item and timeFlag == 1:
+			lanTime.append(item)
+			timeFlag = 0 
+		elif ':' in item and timeFlag == 0:
+			depTime.append(item)
+			timeFlag = 1
+	
 	info = []
-	if len(company) > num-2 and len(punctuality) > num-2 and len(price) > num-2:
+	if len(company) > num-2 and len(punctuality) > num-2 and len(price) > num-2 and len(depTime) > num-2:
 		for i in range(num):
 			if '%' not in punctuality[i]:
 				punctuality[i] = 'NAN'
-			info.append([company[i], punctuality[i], price[i]])
+			info.append([company[i], punctuality[i], price[i], depTime[i], fliTime[i], lanTime[i]])
 	return info
 
 def dataRecord(flightDate, recordNum, fromCity, toCity):
@@ -57,7 +73,7 @@ def dataRecord(flightDate, recordNum, fromCity, toCity):
 		output = open(filename, 'a')
 		temp = 'SearchDate: ' + today + '\n'
 		for item in info:
-			temp += item[0] + '\t' + item[1] + '\t' + item[2] + '\n'
+			temp += item[0] + '\t' + item[1] + '\t' + item[2] + '\t' + item[3] + '\t' + item[4] + '\t' + item[5] + '\n'
 		temp += '\n'
 		output.writelines(temp)
 		output.close()
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+import thread
 from FlightSpider import *
 
 def main():
-	dataRecord('2015-7-15', 5, 'Xiamen', 'Jinan')
+	dataRecord('2015-7-15', 5, 'Guangzhou', 'Jinan')
 
 if __name__ == '__main__':
 	main()
